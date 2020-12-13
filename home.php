7c4a9648d1c2f69a2cf7b7b1d48541dd2de4162d
max van Schoonderwoerd <74652836+MaxvanSchoonderwoerd@users.noreply.github.com>
2020-12-13 13:03:13 +0100

    Created home page, with links to the schedule page and the Planner page. Also created planner.php and rooster.php

--- home.php
+++ home.php
@@ -11,30 +11,31 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login Langetermijnplanning</title>
+    <title>Home Langetermijnplanning</title>
 </head>
 <link rel="stylesheet" href="css/style.css">
 <header>
-    <h1 class="title">Langetermijnplanning</h1>
+    <h1 class="title">Home</h1>
 </header>
 <body>
 <main>
     <section>
         <div>
             <div>
-                <h1>Hallo, wil je inloggen?</h1>
+                <h1>Waar wil je naartoe?</h1>
             </div>
             <div>
                 <div>
-                    <label for="name">Naam:</label>
-                    <input type="text" id="name" name="name" required>
+<!--                    <button type="button">Rooster</button>-->
+                    <form method="get" action="CLE2/rooster.php">
+                        <button type="submit">Rooster</button>
+                    </form>
                 </div>
                 <div>
-                    <label for="password">Wachtwoord:</label>
-                    <input type="password" id="password" name="password" required>
-                </div>
-                <div>
-                    <button type="submit">Versturen</button>
+<!--                    <button type="button">Planner</button>-->
+                    <form method="post" action="planner.php">
+                        <button type="submit">Planner</button>
+                    </form>
                 </div>
             </div>
         </div>
---
+++ planner.php
@@ -0,0 +1,85 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" type="image/png" href="favicon.ico"/>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home Langetermijnplanning</title>
+</head>
+<link rel="stylesheet" href="css/style.css">
+<header>
+    <!--    <h1 class="title">Planner</h1>-->
+    <nav>
+        <div>
+            <form method="post" action="home.php">
+                <button type="submit">Home</button>
+            </form>
+        </div>
+    </nav>
+</header>
+<body>
+<main>
+    <section>
+        <div>
+            <div>
+                <h1>Vul hier de gegevens in</h1>
+            </div>
+            <div>
+                <div>
+                    <label for="name">Naam:</label>
+                    <input type="text" id="name" name="name" required>
+                    </form>
+                </div>
+                <div>
+                    <label for="date">Geboorte datum:</label>
+                    <input type="date" id="date" name="date" required>
+                </div>
+                <div>
+                    <label for="monday">Maandag: </label>
+                    <input type="checkbox" id="monday" name="monday">
+                </div>
+                <div>
+                    <label for="tuesday">Dinsdag: </label>
+                    <input type="checkbox" id="tuesday" name="tuesday">
+                </div>
+                <div>
+                    <label for="wednesday">Woensdag: </label>
+                    <input type="checkbox" id="wednesday" name="wednesday">
+                </div>
+                <div>
+                    <label for="thursday">Donderdag: </label>
+                    <input type="checkbox" id="thursday" name="thursday">
+                </div>
+                <div>
+                    <label for="friday">Vrijdag: </label>
+                    <input type="checkbox" id="friday" name="friday">
+                </div>
+                <div>
+                    <label for="saturday">Zaterdag: </label>
+                    <input type="checkbox" id="saturday" name="saturday">
+                </div>
+                <div>
+                    <label for="sunday">Zondag: </label>
+                    <input type="checkbox" id="sunday" name="sunday">
+                </div>
+                <div>
+                    <button type="submit">Plannen</button>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <right>
+        <img src="source/CLE2_Logo.png">
+    </right>
+</main>
+
+</body>
+</html>
